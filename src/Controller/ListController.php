b8df9398a9df5e5fd830c32120584a0574994d72
DimaArtefakt <dima.artefakt@mail.ru>
2022-01-28 00:28:42 +0300

    add list user

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ListController extends AbstractController
 {
-    #[Route('/list', name: 'list')]
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/listid', name: 'listid')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -17,5 +25,20 @@ class ListController extends AbstractController
         return $this->render('list/index.html.twig', [
             'controller_name' => 'ListController','error' => $error
         ]);
+    }
+
+
+    #[Route('/list', name: 'list')]
+    public function show(AuthenticationUtils $authenticationUtils): Response
+    {
+        $user = $this->doctrine->getRepository(User::class)->findAll();
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('list/index.html.twig', [
+            'users' => $user,'error' => $error
+        ]);
+
+
     }
 }
